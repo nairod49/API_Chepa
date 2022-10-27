61abef3ce5fcfe3108808a45bc7e894f7d3c0814
dominikTanko <dominik.tanko23@gmail.com>
2022-10-27 15:38:44 +0200

    Fix merge conflicts

--- chepa-front/src/app/app.component.ts
+++ chepa-front/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MediaObserver, MediaChange } from '@angular/flex-layout';
 import { Subscription } from 'rxjs';
-import { Component } from '@angular/core';
-import { GetApiService } from './get-api.service';
+import { GetApiService } from 'get-api.service';
 
 @Component({
   selector: 'app-root',
